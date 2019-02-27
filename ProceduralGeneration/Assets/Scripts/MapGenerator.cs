0998bdfab3b22d9c2aa38ed838779d385b18eaf7
Johan Croix Plasencia Estrada <alu0101013534@ull.edu.es>
2019-02-27 23:49:39 +0000

    noise map

---
+++ ProceduralGeneration/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MapGenerator : MonoBehaviour {
+
+
+    public int mapWidht;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noisemap = Noise.GenerateNoiseMap(mapWidht, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noisemap);
+    }
+}
